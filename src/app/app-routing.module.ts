edde6ea3ae4b6939b66dfdaee4ca4b7e00878a84
WESLEY FELIPE DA SILVA <wesley.fsilva28@senacsp.edu.br>
2020-03-11 22:20:31 -0300

    criação de páginas formulário e edição das tarefas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'task-list', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  {
-    path: 'task-list',
-    loadChildren: () => import('./tasks/task-list/task-list.module').then( m => m.TaskListPageModule)
-  },
-  {
-    path: 'task-list-item',
-    loadChildren: () => import('./tasks/task-list-item/task-list-item.module').then( m => m.TaskListItemPageModule)
-  },
-  {
-    path: 'task-form',
-    loadChildren: () => import('./tasks/task-form/task-form.module').then( m => m.TaskFormPageModule)
-  },
+  { path: 'task-list', loadChildren: './tasks/task-list/task-list.module#TaskListPageModule'},
+  { path: 'new', loadChildren: './tasks/task-form/task-form.module#TaskFormPageModule'},
+  { path: 'edit/:id', loadChildren: './tasks/task-form/task-form.module#TaskFormPageModule'},
 ];
 
 @NgModule({
--- src/app/tasks/task-list/task-list.page.ts
+++ src/app/tasks/task-list/task-list.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from './../shared/task.service';
+import { Task } from './../shared/task';
 
 @Component({
   selector: 'app-task-list',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-list.page.scss'],
 })
 export class TaskListPage implements OnInit {
+  tasks: Task [] = [];
 
-  constructor() { }
+  constructor(private TaskService: TaskService) { }
 
   ngOnInit() {
+    this.tasks = this.TaskService.getAll();
   }
 
 }
